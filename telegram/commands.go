a9ccff3a71330ec2925c98651711a7e784fd6af0
ChernyshevVyacheslav <Chernyshev2001@icloud.com>
2021-08-16 15:28:57 +0300

    Changed strings for telegram commands

--- telegram/commands.go
+++ telegram/commands.go
@@ -36,19 +36,22 @@ func start(ChatID int64){
 		token := GenerateNewToken()
 		db.AddNewChat(ChatID, token)
 	}
-	message := "Send messages directly into the chat via http post. \n\n /notify — get webhook link for this chat."
+	message := "Send messages directly into the chat via http post. "+"" +
+		"\n\n /notify — get webhook link for this chat."
 	SendMessage(ChatID, message, "HTML", "")
 }
 
 func help(chatID int64){
-	message := "Send notifications to chat easily One step integration. \n\n /notify_start — show webhook link for this chat"
+	message := "Send notifications to chat easily One step integration. "+
+	"\n\n /notify_start — show webhook link for this chat"
 	SendMessage(chatID, message, "HTML", "")
 }
 
 func notify(ChatID int64){
 	if db.IsChatIDInDB(ChatID){
 		token := db.GetToken(ChatID)
-		message := fmt.Sprintf("Use this webhook for sending notifications to the chat:\n\n%s/%s\n\nMake a POST request with text in «message» param.", Url, token)
+		message := fmt.Sprintf("Use this webhook for sending notifications to the chat:\n\n%s/%s"+
+			"\n\nMake a POST request with text in «message» param.", Url, token)
 		SendMessage(ChatID, message, "HTML", "")
 	}else {
 		SendMessage(ChatID, "Print /start", "HTML", "")
